2cde336801281037e339f521f1485ec8fedd59b9
suho-Son <shw103shw@gmail.com>
2025-06-27 08:16:10 +0900

    Externalize user paths

--- backend/source/spring/src/main/java/com/eatc/eatc/mapper/UserMapper.java
+++ backend/source/spring/src/main/java/com/eatc/eatc/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.eatc.eatc.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -14,5 +15,11 @@ public interface UserMapper {
     List<UserDto> findAllUsers();
 
     @Select("SELECT * FROM user_table WHERE login_id = #{loginId} AND password = #{password}")
-    UserDto findByLogin(@Param("loginId") String loginId, @Param("password") String password);   
+    UserDto findByLogin(@Param("loginId") String loginId, @Param("password") String password);
+
+    @Select("SELECT * FROM user_table WHERE user_id = #{id}")
+    UserDto findById(@Param("id") int id);
+
+    @Update("UPDATE user_table SET login_id = #{loginId}, password = #{password}, name = #{name}, phone = #{phone}, email = #{email}, updated_at = CURRENT_TIMESTAMP, status = #{status} WHERE user_id = #{userId}")
+    int updateUser(UserDto userDto);
 }
